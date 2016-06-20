c5c7d9b62acd702d141efd351f6f6fd08a30f44a
Markus Wolf <mail@markus-wolf.de>
2016-06-20 10:36:20 +0200

    refactor: Switches from mocha to ava for test execution (#9)
    * refactor: Switches from mocha to ava for test execution
    * Removed obsolete node deps
    * Remove node_modules path from coverage command
    * Updated postinstall comment

--- test/plugins/commonjs-test.ts
+++ test/plugins/commonjs-test.ts
@@ -1,27 +1,25 @@
-import { assert } from 'chai';
+import test from 'ava';
 import { stripIndent } from 'common-tags';
 import { HostMock, parseAndProcess } from '../helper';
 
 import { reset } from '../../src/modules';
 import { rewriteRequireStatements } from '../../src/plugins/commonjs';
 
-describe('commonjs', () => {
-  beforeEach(() => {
-    reset();
-  });
+test.beforeEach(() => {
+  reset();
+});
 
-  it('should rewrite require statements', () => {
-    const input = stripIndent`
-      var a = require('./dependency');
-    `;
-    const expected = stripIndent`
-      var a = modules[0]().exports;
-    `;
+test('commonjs should rewrite require statements', t => {
+  const input = stripIndent`
+    var a = require('./dependency');
+  `;
+  const expected = stripIndent`
+    var a = modules[0]().exports;
+  `;
 
-    const host = new HostMock({
-      'dependency.js': ''
-    });
-    assert.equal(parseAndProcess(input,
-      ast => rewriteRequireStatements(ast, 'name', [], host)), expected);
+  const host = new HostMock({
+    'dependency.js': ''
   });
+  t.deepEqual(parseAndProcess(input,
+    ast => rewriteRequireStatements(ast, 'name', [], host)), expected);
 });
