d0ebd8593f8744c2ad12b261050344da2a5b9177
Mario Nebl <hello@herebecode.com>
2016-06-21 21:43:58 +0200

    refactor: simplify precedence decision

--- src/module-path.ts
+++ src/module-path.ts
@@ -2,6 +2,34 @@ import { IHost, DefaultHost } from './host';
 import { sync as browserResolveSync } from 'browser-resolve';
 import * as nodeCoreLibs from 'node-libs-browser';
 
+interface IPackage {
+  main: string;
+  browser?: string;
+  'jsnext:main'?: string;
+}
+
+/**
+ * Return package data with normalized main fields
+ * Precedence: browser, jsnext:main, main
+ *
+ * @param pkg Package data
+ */
+function normalizePackage(pkg: IPackage) {
+  // .browser, use package data as is
+  if ('browser' in pkg) {
+    return pkg;
+  }
+
+  // no .browser, .jsnext:main, use jsnext:main by aliasing to .main
+  if ('jsnext:main' in pkg) {
+    pkg.main = pkg['jsnext:main'];
+    return pkg;
+  }
+
+  // use .main
+  return pkg;
+}
+
 /**
  * Return a resolved module path
  *
@@ -13,14 +41,7 @@ export function getModulePath(filename: string, importIdentifier: string, host: 
   return browserResolveSync(importIdentifier, {
     filename: filename,
     modules: nodeCoreLibs,
-    packageFilter: pkg => {
-      // If the browser field is not defined, use jsnext:main instead
-      if ('browser' in pkg === false && 'jsnext:main' in pkg) {
-        pkg.browser = pkg['jsnext:main'];
-      }
-
-      return pkg;
-    },
+    packageFilter: normalizePackage,
     readFileSync: path => new Buffer(host.readFile(path)),
     isFile: filePath => host.fileExists(filePath) && host.isFile(filePath)
   });
