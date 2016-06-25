adb49acb299cc7027f5457e4dea9170d9eded93c
Markus Wolf <mail@markus-wolf.de>
2016-06-25 16:16:13 +0200

    fix: adds a plugin to fix broken AST
    Solves #30 - Remove comments which lead to broken code generation in escodegen

---
+++ test/plugins/test-test.ts
@@ -0,0 +1,41 @@
+import test from 'ava';
+import { runInNewContext } from 'vm';
+import { stripIndent } from 'common-tags';
+import { HostMock, parseAndProcess } from '../helper';
+
+import { reset } from '../../src/modules';
+import { astFixes } from '../../src/plugins/ast-fixes';
+
+function fixComments(input: string, files: any = {}): string {
+  const host = new HostMock(files);
+
+  return parseAndProcess(input, ast => {
+    return astFixes(ast, 'name', host);
+  });
+}
+
+function executeFixComments(input: string, files: any = {}, settings: any = {}): any {
+  const processed = fixComments(input, files);
+  const result = {};
+  runInNewContext(processed, result);
+  return result;
+}
+
+test.beforeEach(reset);
+
+test('test', t => {
+  const input = stripIndent`
+    function a() {
+      return /** **/ function b() {
+        return 'a';
+      }
+    }
+  `;
+
+  const expected = 'a';
+
+  const scope = executeFixComments(input);
+  t.is(typeof scope.a, 'function');
+  t.is(typeof scope.a(), 'function');
+  t.is(scope.a()(), expected);
+});
