fc532f4b858dbbcb80edb29522ac42c23bacf62d
Markus Wolf <mail@markus-wolf.de>
2016-06-27 20:45:55 +0200

    feat: adds 'global' locals __filename and __dirname if required by a module (#50)
    * feat: adds 'global' locals __filename and __dirname if required by a module
    
    fixes #48 and #49 - injects a closure with filename and dirname
    
    * fix: fixes ast manipulation
    
    * refactor: simplified code

---
+++ src/plugins/global-locals.ts
@@ -0,0 +1,44 @@
+import { visit, builders as b, IPath } from 'ast-types';
+
+import { IHost } from '../host';
+
+export function rewriteGlobalLocals(program: ESTree.Program, currentModule: string, host: IHost): void {
+  let detectedFilename = false;
+  let detectedDirname = false;
+  visit(program, {
+    visitIdentifier: function(path: IPath<ESTree.Identifier>): void {
+      if (path.node.name === '__filename' && path.scope.lookup('__filename') === null) {
+        detectedFilename = true;
+      } else if (path.node.name === '__dirname' && path.scope.lookup('__dirname') === null) {
+        detectedDirname = true;
+      }
+      if (detectedFilename && detectedDirname) {
+        this.abort();
+      }
+      this.traverse(path);
+    }
+  });
+
+  if (detectedFilename || detectedDirname) {
+    program.body = [
+      b.expressionStatement(
+        b.callExpression(
+          b.functionExpression(
+            null,
+            [
+              b.identifier('__filename'),
+              b.identifier('__dirname')
+            ],
+            b.blockStatement(
+              program.body
+            )
+          ),
+          [
+            b.literal(currentModule.substring(host.cwd().length)),
+            b.literal(host.dirname(currentModule).substring(host.cwd().length))
+          ]
+        )
+      )
+    ];
+  }
+}
