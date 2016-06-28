2243a0188269485be1f87ca64a0314f6888bfec7
Markus Wolf <mail@markus-wolf.de>
2016-06-29 00:36:32 +0200

    feat: complete temporary cli

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,32 @@ import { bundle } from './bundle';
 // TODO: create config file
 // TODO: add watch mode
 const argv = minimist(process.argv.slice(2), {
-  string: ['config', 'entry', 'source', 'alias', 'runtime'],
+  string: [
+    'config',
+    'entry',
+    'source',
+    'runtime',
+    'out-dir',
+    'out-file',
+    'runtime',
+    'alias'
+  ],
   boolean: ['watch'],
   default: {
     source: 'es2015',
     watch: false
   }
 });
-if (!argv['entry']) {
-  throw new Error('Missing --entry argument');
-}
 
 const startTime = new Date().getTime();
 process.stdout.write(bundle({
   configFile: argv['config'],
   entryPoint: argv['entry'],
   source: argv['source'],
+  outputDirectory: argv['out-dir'],
+  outputFile: argv['out-file'],
+  runtime: argv['runtime'],
+  alias: argv['alias'],
   watchMode: argv['watch']
 }));
 const endTime = new Date().getTime();
