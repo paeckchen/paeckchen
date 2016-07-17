509e680f42612911e583ec82c534ba2655ae0e35
Markus Wolf <mail@markus-wolf.de>
2016-07-17 16:37:06 +0200

    chore(paeckchen): fixes breaking change detection

--- task.js
+++ task.js
@@ -245,7 +245,8 @@ function getNextVersion(packageDir, data) {
     .then(() => {
       const relaseIndex = data.commits.reduce((relase, commit) => {
         let result = relase > (typeToReleaseIndex[commit.type] || 0) ? relase : typeToReleaseIndex[commit.type];
-        if (commit.footer.indexOf('BREAKING CHANGE:\n') > -1) {
+        console.log(commit);
+        if (commit.footer && commit.footer.indexOf('BREAKING CHANGE:\n') > -1) {
           result = 2;
         }
         return result;
