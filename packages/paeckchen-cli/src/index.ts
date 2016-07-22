ff599b2b2f9374afb65e84dcb89661fccc2e326e
Markus Wolf <mail@markus-wolf.de>
2016-07-23 00:32:30 +0200

    feat(paeckchen-core): prepare for source-map writing (#141)

--- packages/paeckchen-cli/src/index.ts
+++ packages/paeckchen-cli/src/index.ts
@@ -6,12 +6,12 @@ import { bundle, DefaultHost, IPaeckchenContext } from 'paeckchen-core';
 
 const startTime = new Date().getTime();
 const options = createOptions(process.argv);
-bundle(options, new DefaultHost(), (result: string, context: IPaeckchenContext) => {
-  if (result) {
+bundle(options, new DefaultHost(), (code: string, souceMap: string|undefined, context: IPaeckchenContext) => {
+  if (code) {
     if (context.config.output.file) {
-      context.host.writeFile(join(context.config.output.folder, context.config.output.file), result);
+      context.host.writeFile(join(context.config.output.folder, context.config.output.file), code);
     } else {
-      process.stdout.write(result);
+      process.stdout.write(code);
     }
   }
   const endTime = new Date().getTime();
