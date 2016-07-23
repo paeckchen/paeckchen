fcb5f4f8d16f05875b041b8a6b28813881274b1b
Markus Wolf <mail@markus-wolf.de>
2016-07-23 10:39:50 +0200

    chore(paeckchen-cli): update and cleanup dependencies

--- packages/paeckchen-cli/typings/execa/execa.d.ts
+++ packages/paeckchen-cli/typings/execa/execa.d.ts
@@ -2,14 +2,14 @@ declare module 'execa' {
   import { ExecFileOptions, ChildProcess } from 'child_process';
   import { Readable } from 'stream';
 
-  interface Thenable {
-    then(handler: (result: ChildProcess) => any): this;
-    catch(handler: (error: Error & {stdout: Readable; stderr: Readable}) => any): this;
-  }
-
-  function execa(file: string, arguments?: string[],
-    options?: ExecFileOptions): ChildProcess & Thenable;
+  function execa(file: string, arguments?: string[], options?: ExecFileOptions): execa.PromiseLike;
   namespace execa {
+
+    interface PromiseLike {
+      then(handler: (result: ChildProcess) => any): this;
+      catch(handler: (error: Error & {stdout: Readable; stderr: Readable}) => any): this;
+    }
+
   }
 
   export = execa;
